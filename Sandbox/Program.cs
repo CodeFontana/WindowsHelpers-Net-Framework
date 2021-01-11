f911f11a10a5a4e40d0af65c3940cc7f3136eff9
Brian Fontana <brian@codefoxtrot.com>
2021-01-10 23:50:48 -0500

    Add Sandbox project to support sanity testing within the library.

---
+++ Sandbox/Program.cs
@@ -0,0 +1,41 @@
+﻿using LoggerLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsLibrary;
+
+namespace Sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Open log.
+            var sandbox = new Logger("Sandbox", null, 52428800, 1);
+
+            // EXAMPLE: Get all columns.
+            sandbox.Log("\n" + WMIHelper.GetFormattedWMIData(
+                sandbox.LogComponent,
+                "root\\cimv2",
+                "Win32_QuickFixEngineering",
+                null));
+
+            // EXAMPLE: Get specified columns.
+            sandbox.Log("\n" + WMIHelper.GetFormattedWMIData(
+                sandbox.LogComponent,
+                "root\\cimv2",
+                "Win32_QuickFixEngineering",
+                new List<string> { "HotFixID", "Description", "InstalledOn", "Caption" },
+                4));
+
+            // Close log.
+            sandbox.Close();
+
+            // Press any key to continue.
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
+        }
+    }
+}
